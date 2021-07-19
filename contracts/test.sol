13f2fbc778ffa18ac1b75027d81c709bc82bc823
29449 <2944972991@qq.com>
2021-07-19 14:53:25 +0800

    流动性添加成功

--- contracts/test.sol
+++ contracts/test.sol
@@ -1,5 +1,6 @@
 // SPDX-License-Identifier: Unlicensed
 pragma solidity ^0.6.12;
+
 interface IERC20 {
 
     function totalSupply() external view returns (uint256);
@@ -628,12 +629,6 @@ interface IUniswapV2Router01 {
 
 interface IUniswapV2Router02 is IUniswapV2Router01 {
     function removeLiquidityETHSupportingFeeOnTransferTokens(
-        address token,
-        uint liquidity,
-        uint amountTokenMin,
-        uint amountETHMin,
-        address to,
-        uint deadline
     ) external returns (uint amountETH);
 
     function removeLiquidityETHWithPermitSupportingFeeOnTransferTokens(
@@ -670,32 +665,370 @@ interface IUniswapV2Router02 is IUniswapV2Router01 {
     ) external;
 }
 
-contract swapTest is  Ownable  {
+contract swapTest is Context, IERC20, Ownable {
     using SafeMath for uint256;
 
-    //代币数量 以太币数量
-    function addLiquidity(address uniswapV2Router,address tokenAddr,uint256 tokenAmount,uint256 a,uint256 b)payable public {
-        IERC20(tokenAddr).approve(uniswapV2Router,~uint256(0));
-        IUniswapV2Router02(uniswapV2Router).addLiquidityETH{value : msg.value}(
-            tokenAddr,
-            tokenAmount,
-            a, // slippage is unavoidable
-            b, // slippage is unavoidable
-            owner(),
-            block.timestamp
-        );
+    mapping(address => uint256) private _balances;
+    mapping(address => mapping(address => uint256)) private _allowances;
+    mapping(address => bool) private _exclude;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+    uint256 private _decimals;
+    uint256 private lpFee;
+    uint256 private txFee;
+    uint256 private deFee;
+    IUniswapV2Router02 public immutable uniswapV2Router;
+    /**
+     * @dev Sets the values for {name} and {symbol}, initializes {decimals} with
+     * a default value of 18.
+     *
+     * To select a different value for {decimals}, use {_setupDecimals}.
+     *
+     * All three of these values are immutable: they can only be set once during
+     * construction.
+     */
+    constructor (
+        string memory name_,
+        string memory symbol_,
+        uint256 decimals_,
+        uint256 count_,
+        uint256 lpFee_,
+        uint256 txFee_,
+        uint256 deFee_,
+        IUniswapV2Router02 uniswapV2Router_)
+    public {
+        _name = name_;
+        _symbol = symbol_;
+        _decimals = decimals_;
+        lpFee = lpFee_;
+        txFee = txFee_;
+        deFee = deFee_;
+        _owner = _msgSender();
+        _exclude[_msgSender()] = true;
+        _exclude[address(this)] = true;
+        uniswapV2Router = uniswapV2Router_;
+        _mint(_msgSender(), count_ * 10 ** _decimals);
+    }
+
+    /**
+     * @dev Returns the name of the token.
+     */
+    function name() public view virtual returns (string memory) {
+        return _name;
+    }
+
+    /**
+     * @dev Returns the symbol of the token, usually a shorter version of the
+     * name.
+     */
+    function symbol() public view virtual returns (string memory) {
+        return _symbol;
+    }
+
+    /**
+     * @dev Returns the number of decimals used to get its user representation.
+     * For example, if `decimals` equals `2`, a balance of `505` tokens should
+     * be displayed to a user as `5,05` (`505 / 10 ** 2`).
+     *
+     * Tokens usually opt for a value of 18, imitating the relationship between
+     * Ether and Wei. This is the value {ERC20} uses, unless {_setupDecimals} is
+     * called.
+     *
+     * NOTE: This information is only used for _display_ purposes: it in
+     * no way affects any of the arithmetic of the contract, including
+     * {IERC20-balanceOf} and {IERC20-transfer}.
+     */
+    function decimals() public view virtual returns (uint8) {
+        return _decimals;
+    }
+
+    /**
+     * @dev See {IERC20-totalSupply}.
+     */
+    function totalSupply() public view virtual override returns (uint256) {
+        return _totalSupply;
+    }
+
+    /**
+     * @dev See {IERC20-balanceOf}.
+     */
+    function balanceOf(address account) public view virtual override returns (uint256) {
+        return _balances[account];
+    }
+
+    /**
+     * @dev See {IERC20-transfer}.
+     *
+     * Requirements:
+     *
+     * - `recipient` cannot be the zero address.
+     * - the caller must have a balance of at least `amount`.
+     */
+    function transfer(address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(_msgSender(), recipient, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-allowance}.
+     */
+    function allowance(address owner, address spender) public view virtual override returns (uint256) {
+        return _allowances[owner][spender];
+    }
+
+    /**
+     * @dev See {IERC20-approve}.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function approve(address spender, uint256 amount) public virtual override returns (bool) {
+        _approve(_msgSender(), spender, amount);
+        return true;
+    }
+
+    /**
+     * @dev See {IERC20-transferFrom}.
+     *
+     * Emits an {Approval} event indicating the updated allowance. This is not
+     * required by the EIP. See the note at the beginning of {ERC20}.
+     *
+     * Requirements:
+     *
+     * - `sender` and `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     * - the caller must have allowance for ``sender``'s tokens of at least
+     * `amount`.
+     */
+    function transferFrom(address sender, address recipient, uint256 amount) public virtual override returns (bool) {
+        _transfer(sender, recipient, amount);
+        _approve(sender, _msgSender(), _allowances[sender][_msgSender()].sub(amount, "ERC20: transfer amount exceeds allowance"));
+        return true;
+    }
+
+    /**
+     * @dev Atomically increases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     */
+    function increaseAllowance(address spender, uint256 addedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].add(addedValue));
+        return true;
+    }
+
+    /**
+     * @dev Atomically decreases the allowance granted to `spender` by the caller.
+     *
+     * This is an alternative to {approve} that can be used as a mitigation for
+     * problems described in {IERC20-approve}.
+     *
+     * Emits an {Approval} event indicating the updated allowance.
+     *
+     * Requirements:
+     *
+     * - `spender` cannot be the zero address.
+     * - `spender` must have allowance for the caller of at least
+     * `subtractedValue`.
+     */
+    function decreaseAllowance(address spender, uint256 subtractedValue) public virtual returns (bool) {
+        _approve(_msgSender(), spender, _allowances[_msgSender()][spender].sub(subtractedValue, "ERC20: decreased allowance below zero"));
+        return true;
+    }
+
+    /**
+     * @dev Moves tokens `amount` from `sender` to `recipient`.
+     *
+     * This is internal function is equivalent to {transfer}, and can be used to
+     * e.g. implement automatic token fees, slashing mechanisms, etc.
+     *
+     * Emits a {Transfer} event.
+     *
+     * Requirements:
+     *
+     * - `sender` cannot be the zero address.
+     * - `recipient` cannot be the zero address.
+     * - `sender` must have a balance of at least `amount`.
+     */
+    function _transfer(address sender, address recipient, uint256 amount) internal virtual {
+        require(sender != address(0), "ERC20: transfer from the zero address");
+        require(recipient != address(0), "ERC20: transfer to the zero address");
+        _beforeTokenTransfer(sender, recipient, amount);
+        (uint256 lpFees, uint256 lp) = calculateLiquidityFee(amount, _exclude[sender]);
+        (uint256 deFees) = calculateDestroyFee(amount, _exclude[sender]);
+        _balances[sender] = _balances[sender].sub(amount, "ERC20: transfer amount exceeds balance");
+        _balances[address(this)] = _balances[address(this)].add(lpFees);
+        _balances[address(0)] = _balances[address(0)].add(deFees);
+        _balances[recipient] = _balances[recipient].add(lp.sub(deFees));
+        if (!_exclude[sender]) {
+            swapTokensForEth(lpFees / 2);
+            uint256 initialBalance = address(this).balance;
+            addLiquidity(lpFees / 2, initialBalance);
+        }
+        emit Transfer(sender, recipient, lpFeess);
+
+    }
+
+    /** @dev Creates `amount` tokens and assigns them to `account`, increasing
+     * the total supply.
+     *
+     * Emits a {Transfer} event with `from` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `to` cannot be the zero address.
+     */
+    function _mint(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: mint to the zero address");
+
+        _beforeTokenTransfer(address(0), account, amount);
+
+        _totalSupply = _totalSupply.add(amount);
+        _balances[account] = _balances[account].add(amount);
+        emit Transfer(address(0), account, amount);
+    }
+
+    /**
+     * @dev Destroys `amount` tokens from `account`, reducing the
+     * total supply.
+     *
+     * Emits a {Transfer} event with `to` set to the zero address.
+     *
+     * Requirements:
+     *
+     * - `account` cannot be the zero address.
+     * - `account` must have at least `amount` tokens.
+     */
+    function _burn(address account, uint256 amount) internal virtual {
+        require(account != address(0), "ERC20: burn from the zero address");
+
+        _beforeTokenTransfer(account, address(0), amount);
+
+        _balances[account] = _balances[account].sub(amount, "ERC20: burn amount exceeds balance");
+        _totalSupply = _totalSupply.sub(amount);
+        emit Transfer(account, address(0), amount);
+    }
+
+    /**
+     * @dev Sets `amount` as the allowance of `spender` over the `owner` s tokens.
+     *
+     * This internal function is equivalent to `approve`, and can be used to
+     * e.g. set automatic allowances for certain subsystems, etc.
+     *
+     * Emits an {Approval} event.
+     *
+     * Requirements:
+     *
+     * - `owner` cannot be the zero address.
+     * - `spender` cannot be the zero address.
+     */
+    function _approve(address owner, address spender, uint256 amount) internal virtual {
+        require(owner != address(0), "ERC20: approve from the zero address");
+        require(spender != address(0), "ERC20: approve to the zero address");
+
+        _allowances[owner][spender] = amount;
+        emit Approval(owner, spender, amount);
+    }
+
+    /**
+     * @dev Sets {decimals} to a value other than the default one of 18.
+     *
+     * WARNING: This function should only be called from the constructor. Most
+     * applications that interact with token contracts will not expect
+     * {decimals} to ever change, and may work incorrectly if it does.
+     */
+    function _setupDecimals(uint8 decimals_) internal virtual {
+        _decimals = decimals_;
+    }
+
+    /**
+     * @dev Hook that is called before any transfer of tokens. This includes
+     * minting and burning.
+     *
+     * Calling conditions:
+     *
+     * - when `from` and `to` are both non-zero, `amount` of ``from``'s tokens
+     * will be to transferred to `to`.
+     * - when `from` is zero, `amount` tokens will be minted for `to`.
+     * - when `to` is zero, `amount` of ``from``'s tokens will be burned.
+     * - `from` and `to` are never both zero.
+     *
+     * To learn more about hooks, head to xref:ROOT:extending-contracts.adoc#using-hooks[Using Hooks].
+     */
+    function _beforeTokenTransfer(address from, address to, uint256 amount) internal virtual {}
+
+    //==============================================================================
+
+    /**
+    * @dev 计算流动性费用
+    * @param _amount 转账总数
+    * @param exclude 是否排除
+    */
+    function calculateLiquidityFee(uint256 _amount, bool exclude) private view returns (uint256, uint256) {
+        if (exclude) {
+            return (0, _amount);
+        } else {
+            //_数量 * _流动资金费 / 100
+            uint256 lp = _amount.mul(lpFee).div(10 ** 2);
+            return (lp, _amount.sub(lp));
+
+        }
+    }
+
+    /**
+    * @dev 计算销毁费用
+    * @param _amount 转账总数
+    * @param exclude 是否排除
+    */
+    function calculateDestroyFee(uint256 _amount, bool exclude) private view returns (uint256) {
+        if (exclude) {
+            return (0);
+        } else {
+            //_数量 * _流动资金费 / 100
+            uint256 de = _amount.mul(deFee).div(10 ** 2);
+            return (de);
+
+        }
     }
 
 
+    //将代币换成 Eth
     //代币数量
-    function addLiquiditys(address uniswapV2Router, uint256 tokenAmount, uint256 tokenBmount,address tokenAddr,address tokenBddr) public {
-        IERC20(tokenAddr).approve(uniswapV2Router,~uint256(0));
-        IERC20(tokenBddr).approve(uniswapV2Router,~uint256(0));
-        IUniswapV2Router02(uniswapV2Router).addLiquidity(
-            tokenAddr,
-            tokenBddr,
+    function swapTokensForEth(uint256 tokenAmount) private {
+        address[] memory path = new address[](2);
+        path[0] = address(this);
+        path[1] = uniswapV2Router.WETH();
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+        uniswapV2Router.swapExactTokensForETHSupportingFeeOnTransferTokens(
             tokenAmount,
-            tokenBmount,
+            0, // accept any amount of ETH
+            path,
+            address(this),
+            block.timestamp
+        );
+    }
+
+
+    //增加流动性
+    //代币数量 以太币数量
+    function addLiquidity(uint256 tokenAmount, uint256 ethAmount) private {
+        // approve token transfer to cover all possible scenarios 批准令牌转移以涵盖所有可能的情况
+        //_批准(当前合约，address(uniswapV2Router),代币数量)
+        _approve(address(this), address(uniswapV2Router), tokenAmount);
+
+        // add the liquidity 增加流动性
+        // uniswapV2Router.添加流动性 ETH{value:以太币数量}(当前合约，代币数量，0，0，当前合约拥有者，当前区块时间戳)
+        uniswapV2Router.addLiquidityETH{value : ethAmount}(
+            address(this),
+            tokenAmount,
             0, // slippage is unavoidable
             0, // slippage is unavoidable
             owner(),
@@ -703,6 +1036,5 @@ contract swapTest is  Ownable  {
         );
     }
 
+    receive() external payable {}
 }
-
-
